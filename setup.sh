bf85e3c31a92f5d29168a3e88a9e53974ca6c5d7
Umberto <arduiniumberto@gmail.com>
2026-02-20 19:02:55 +0100

    Add sudo to cp command in setup.sh for proper permissions

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ wget https://github.com/bluenviron/mediamtx/releases/download/v1.16.0/mediamtx_v
 # Extract the archive (excluding the default config to preserve our custom one)
 tar -xvzf mediamtx_v1.16.0_linux_arm64.tar.gz --exclude=mediamtx.yml
 
-cp systemd/secure-camera.service /etc/systemd/system/secure-camera.service
+sudo cp systemd/secure-camera.service /etc/systemd/system/secure-camera.service
 sudo systemctl daemon-reload
 sudo systemctl enable secure-camera.service
 sudo systemctl start secure-camera.service
